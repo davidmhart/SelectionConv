b20ac02792668037379c72f43251dbf8c1993059
David <davidhart100@gmail.com>
2022-07-18 12:24:43 -0600

    Cleaned up repo for project page, fixed 360 images

--- scripts/config.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-sys.path.append(os.path.split(os.path.dirname(__file__))[0])
-
-imagenet_directory = "D:/ImageNet_data/"
-cifar_directory = "cifar10_data/"
--- segmentation_eval/config.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-#sys.path.append(os.path.split(os.path.dirname(__file__))[0])
-
-stanford_dir = "/media/david/Seagate Expansion Drive/Datasets/Stanford2D-3DS/"
-#stanford_dir = "/Data6/david/Stanford2d3ds/"
-pano_mask = "pano-mask.png"
-output_dir = "segmentation_eval/outputs/"
